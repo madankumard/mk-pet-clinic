6a9a0471aff1eed7ed4ba1231bf105146e5f3b07
MadanKumar <madan739@gmail.com>
2020-06-03 13:07:23 +0530

    Starter init project commit

---
+++ src/main/java/mk/springframework/mkpetclinic/MkPetClinicApplication.java
@@ -0,0 +1,13 @@
+package mk.springframework.mkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkPetClinicApplication.class, args);
+	}
+
+}
