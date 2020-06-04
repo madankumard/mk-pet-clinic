53aae910ed4955255df11dfcbbbdd4ff8d0e1627
MadanKumar <madan739@gmail.com>
2020-06-04 16:57:41 +0530

    added starter services

---
+++ pet-clinic-data/src/main/java/mk/springframework/mkpetclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package mk.springframework.mkpetclinic.services;
+
+import mk.springframework.mkpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
