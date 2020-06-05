4c4a038aac9b42bf49f2aefe7cbf9d9f6828c6d9
MadanKumar <madan739@gmail.com>
2020-06-05 08:00:44 +0530

    added common interface

--- pet-clinic-data/src/main/java/mk/springframework/mkpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/mk/springframework/mkpetclinic/services/PetService.java
@@ -2,10 +2,6 @@ package mk.springframework.mkpetclinic.services;
 
 import mk.springframework.mkpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/mk/springframework/mkpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/mk/springframework/mkpetclinic/services/VetService.java
@@ -2,10 +2,6 @@ package mk.springframework.mkpetclinic.services;
 
 import mk.springframework.mkpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
