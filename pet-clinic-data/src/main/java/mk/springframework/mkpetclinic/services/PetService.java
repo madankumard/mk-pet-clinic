35e8030f2a978be25df11c0e5d20eb795b24eff8
MadanKumar <madan739@gmail.com>
2020-06-06 08:28:12 +0530

    Added UI for vet and owner

--- pet-clinic-data/src/main/java/mk/springframework/mkpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/mk/springframework/mkpetclinic/services/PetService.java
@@ -4,4 +4,10 @@ import mk.springframework.mkpetclinic.model.Pet;
 
 public interface PetService extends CrudService<Pet, Long> {
 
+    public default void me1(){
+        System.out.println("hello1");
+    }
+    public default void me2(){
+        System.out.println("hello1");
+    }
 }
