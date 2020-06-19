c6737ad281210a0cc4f15d58fe722ce76e3fccbe
MadanKumar <madan739@gmail.com>
2020-06-19 19:22:36 +0530

    Implement Find Owner - UI

--- pet-clinic-data/src/main/java/mk/springframework/mkpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/mk/springframework/mkpetclinic/services/OwnerService.java
@@ -2,6 +2,9 @@ package mk.springframework.mkpetclinic.services;
 
 import mk.springframework.mkpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long>{
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String lastName);
 }
